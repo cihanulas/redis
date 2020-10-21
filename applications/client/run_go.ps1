59ad2b38860ff2796e5428bc6797b820cefc6ce9
Cihan Ulas <culas@isravision.com>
2020-10-21 15:18:50 +0300

    run after build.

--- applications/client/run_go.ps1
+++ applications/client/run_go.ps1
@@ -1,1 +1,8 @@
-docker run -it -v ${PWD}:/go/src -w /go/src --net redis -p 80:80 golang:1.14-alpine
+# docker run -it -v ${PWD}:/go/src -w /go/src --net redis -p 80:80 golang:1.14-alpine
+
+docker run -it --net redis `
+    -e REDIS_HOST=redis `
+    -e REDIS_PORT=6379 `
+    -e REDIS_PASSWORD="my_secure_password" `
+    -p 80:80 `
+    cihanulas/go-client:v1.0.0
